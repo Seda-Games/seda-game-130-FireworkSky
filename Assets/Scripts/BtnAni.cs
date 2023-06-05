a4bc24f0919f746739d8087d96b89937d461df1d
geoffreyjiejie <906907167@qq.com>
2023-06-05 17:05:55 +0800

    按钮增加动画

---
+++ Assets/Scripts/BtnAni.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class BtnAni : MonoBehaviour
+{
+    Button BTN;
+    const float time = 0.08f;
+    bool isOnBtn = false;
+    private void Start()
+    {
+        BTN = GetComponent<Button>();
+        BTN.onClick.AddListener(AnitoButton);
+    }
+    public void AnitoButton()
+    {
+        // this.transform.DOKill();
+        if (!BTN.interactable) return;
+        if (isOnBtn) return;
+        isOnBtn = true;
+        this.transform.DOScale(0.9f, time).SetEase(Ease.Linear).OnComplete(() =>
+        {
+            this.transform.DOScale(1f, time).SetEase(Ease.Linear).OnComplete(() =>
+            {
+                isOnBtn = false;
+            });
+        });
+    }
+    private void OnDestroy()
+    {
+        BTN?.onClick.RemoveAllListeners();
+    }
+    //·Å´ó
+    public void Amplify()
+    {
+        this.transform.DOScale(0.9f, time).SetEase(Ease.Linear);
+    }
+    //»Øµ¯
+    public void Reduce()
+    {
+        this.transform.DOScale(1f, time).SetEase(Ease.Linear);
+    }
+}

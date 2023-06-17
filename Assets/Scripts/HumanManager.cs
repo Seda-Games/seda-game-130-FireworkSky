98e3401eabcf02684d1a32c1a3b03b5cf424fee2
geoffreyjiejie <906907167@qq.com>
2023-06-17 15:56:34 +0800

    模型大小调整

--- Assets/Scripts/HumanManager.cs
+++ Assets/Scripts/HumanManager.cs
@@ -40,9 +40,27 @@ public class HumanManager : MonoBehaviour
         visitorLevel = Mathf.Clamp(visitorLevel, G.dc.gd.humanDatas[0].level, G.dc.gd.humanDatas[G.dc.gd.humanDatas.Length - 1].level);
         if (timeLine > G.dc.gd.humanDataDataDict[visitorLevel].second)
         {
-            
+
             //int stage = PlayerPrefs.GetInt(G.STAGE, 1)-1;
-            StartCoroutine(Visitor());
+            switch (G.dc.gd.levelDict[PlayerPrefs.GetInt(G.MAP, 1)].mapId)
+            {
+                case 1:
+                    StartCoroutine(Visitor());
+                    break;
+                case 2:
+                    StartCoroutine(Boat());
+                    break;
+                case 3:
+                    StartCoroutine(Visitor());
+                    break;
+                case 4:
+                    StartCoroutine(Boat());
+                    break;
+                case 5:
+                    StartCoroutine(Visitor());
+                    break;
+                
+            }
             timeLine = 0;
         }
         /*if (isachieve == true)
@@ -79,9 +97,9 @@ public class HumanManager : MonoBehaviour
             
             // 随机生成一个区域内的点
             Vector3 targetPoint = new Vector3(
-                Random.Range(areaCenter[0].x - areaSize[0].x / 2, areaCenter[0].x + areaSize[0].x / 2),
-                Random.Range(areaCenter[0].y - areaSize[0].y / 2, areaCenter[0].y + areaSize[0].y / 2),
-                Random.Range(areaCenter[0].z - areaSize[0].z / 2, areaCenter[0].z + areaSize[0].z / 2)
+                Random.Range(areaCenter[1].x - areaSize[1].x / 2, areaCenter[1].x + areaSize[1].x / 2),
+                Random.Range(areaCenter[1].y - areaSize[1].y / 2, areaCenter[1].y + areaSize[1].y / 2),
+                Random.Range(areaCenter[1].z - areaSize[1].z / 2, areaCenter[1].z + areaSize[1].z / 2)
             );
             float duration = Vector3.Distance(characterObj.transform.position, targetPoint) / 2f;
             if (PlayerPrefs.GetInt(G.ACHIEVEMENTHUMANSTAGE, 1) == G.dc.gd.achievementTables.Length + 1)
@@ -194,6 +212,137 @@ public class HumanManager : MonoBehaviour
             yield return new WaitForSeconds(1f);
         }
         
+    }
+
+    IEnumerator Boat()
+    {
+
+        for (int i = 0; i < G.dc.gd.humanDataDataDict[visitorLevel - 1].flow; i++)
+        {
+
+            int number = Random.Range(0, boatPrefab.Length);
+            // 在出生点生成一个人物
+            GameObject characterObj = Instantiate(boatPrefab[number], spawnPoint[0], Quaternion.identity);
+            humanPrefab.Add(characterObj);
+            Animator animator = characterObj.transform.GetChild(0).GetComponent<Animator>();
+
+            // 随机生成一个区域内的点
+            Vector3 targetPoint = new Vector3(
+                Random.Range(areaCenter[0].x - areaSize[0].x / 2, areaCenter[0].x + areaSize[0].x / 2),
+                Random.Range(areaCenter[0].y - areaSize[0].y / 2, areaCenter[0].y + areaSize[0].y / 2),
+                Random.Range(areaCenter[0].z - areaSize[0].z / 2, areaCenter[0].z + areaSize[0].z / 2)
+            );
+            float duration = Vector3.Distance(characterObj.transform.position, targetPoint) / 2f;
+            if (PlayerPrefs.GetInt(G.ACHIEVEMENTHUMANSTAGE, 1) == G.dc.gd.achievementTables.Length + 1)
+            {
+                isachieve = true;
+            }
+            else
+            {
+                human += 1;
+                PlayerPrefs.SetInt(G.ACHIEVEMENTHUMAN, human);
+                stage1 = Mathf.Clamp(PlayerPrefs.GetInt(G.ACHIEVEMENTHUMANSTAGE, 1), G.dc.gd.achievementTables[0].level, G.dc.gd.achievementTableDict[G.dc.gd.achievementTables.Length].level);
+                if (PlayerPrefs.GetInt(G.ACHIEVEMENTHUMAN, human) >= G.dc.gd.achievementTableDict[stage1].accumulatehuman)
+                {
+                    isfinish = true;
+                    GameManager.instance.playUI.huamanRewardButton.gameObject.SetActive(true);
+                    GameManager.instance.playUI.UnRewardHuman.gameObject.SetActive(false);
+
+                    if (GameManager.instance.playUI.isrewardhuman == true)
+                    {
+                        stage1 += 1;
+                        PlayerPrefs.SetInt(G.ACHIEVEMENTHUMANSTAGE, stage1);
+                        isachieve = true;
+                        human = 0;
+                        PlayerPrefs.SetInt(G.ACHIEVEMENTHUMAN, human);
+                        isfinish = false;
+                    }
+
+                }
+                else
+                {
+                    GameManager.instance.playUI.isrewardhuman = false;
+                    GameManager.instance.playUI.huamanRewardButton.gameObject.SetActive(false);
+                    GameManager.instance.playUI.UnRewardHuman.gameObject.SetActive(true);
+                }
+            }
+
+
+            GameManager.instance.playUI.UpdateHumanNumber(PlayerPrefs.GetInt(G.ACHIEVEMENTHUMANSTAGE, stage1));
+
+
+            // 使用 DOTween 让人物移动到目标点
+            // 使用 DOTween 让人物始终面向移动方向
+            characterObj.transform.DOLookAt(targetPoint, 0.1f);
+            characterObj.transform.DOMove(targetPoint, duration).SetEase(Ease.Linear).OnComplete(() =>
+            {
+                //animator.SetTrigger("WalkToIdle1");
+                int maxlevel = 0;
+                foreach (var item in GameManager.instance.firePlaneManager.firePlanes)
+                {
+                    if (item.fireWork != null)
+                    {
+                        if (item.fireWork.curFireworkLevel > maxlevel)
+                        {
+                            maxlevel = item.fireWork.curFireworkLevel;
+                        }
+                    }
+                }
+                //characterObj.GetComponent<Human>().curLevel = G.dc.gd.humanDataDataDict[visitorLevel - 1].level;
+                characterObj.GetComponent<Human>().curIncome = G.dc.gd.fireWorkDataDict[maxlevel].humanincome;
+                //characterObj.transform.DORotate(watchPoint.eulerAngles, 1).SetEase(Ease.Linear).OnComplete(() =>
+                //{
+                    Debug.Log("dwqdqwdqwdqwdqwdqd");
+                    number = Random.Range(0, GameSceneManager.Instance.sceneCanvas.emojiObj.Length);
+                    GameSceneManager.Instance.sceneCanvas.EmojiPool = new GameObjectPool(GameSceneManager.Instance.sceneCanvas.emojiObj[number]);
+                    GameSceneManager.Instance.sceneCanvas.emojiObj[number].transform.forward = CameraManager.Instance.transform.forward;
+                    GameSceneManager.Instance.sceneCanvas.ShowEmoji(characterObj.transform.position + new Vector3(0, 0.2f, 0));
+                    // 让人物在目标点停留10秒
+                    DOVirtual.DelayedCall(10f, () =>
+                    {
+                        if (isachieve == true)
+                        {
+                            Debug.Log("现在的人数等级是多少" + PlayerPrefs.GetInt(G.ACHIEVEMENTHUMANSTAGE, 1));
+                            if (PlayerPrefs.GetInt(G.ACHIEVEMENTHUMANSTAGE, 1) == G.dc.gd.achievementTables.Length + 1)
+                            {
+                                GameManager.instance.AddMoney(characterObj.GetComponent<Human>().curIncome * G.dc.gd.achievementTableDict[PlayerPrefs.GetInt(G.ACHIEVEMENTHUMANSTAGE, 1) - 1].multiple);
+                                GameSceneManager.Instance.sceneCanvas.ShowMoneyText(characterObj.transform.position + Vector3.up, characterObj.GetComponent<Human>().curIncome * G.dc.gd.achievementTableDict[PlayerPrefs.GetInt(G.ACHIEVEMENTHUMANSTAGE, 1) - 1].multiple);
+                            }
+                            else
+                            {
+                                int num = Mathf.Clamp(PlayerPrefs.GetInt(G.ACHIEVEMENTHUMANSTAGE, 1), G.dc.gd.achievementTables[0].level, G.dc.gd.achievementTableDict[G.dc.gd.achievementTables.Length].level);
+                                GameManager.instance.AddMoney(characterObj.GetComponent<Human>().curIncome * G.dc.gd.achievementTableDict[num - 1].multiple);
+                                GameSceneManager.Instance.sceneCanvas.ShowMoneyText(characterObj.transform.position + Vector3.up, characterObj.GetComponent<Human>().curIncome * G.dc.gd.achievementTableDict[num - 1].multiple);
+                            }
+
+
+                        }
+                        else
+                        if (isachieve == false)
+                        {
+                            GameManager.instance.AddMoney(characterObj.GetComponent<Human>().curIncome);
+                            GameSceneManager.Instance.sceneCanvas.ShowMoneyText(characterObj.transform.position + Vector3.up, characterObj.GetComponent<Human>().curIncome);
+                        }
+                        animator.SetTrigger("Walk");
+                        // 使用 DOTween 让人物始终面向移动方向
+                        characterObj.transform.DOLookAt(destroyPoint[0], 0.1f).OnComplete(() =>
+                        {
+                            // 计算移动时间
+                            float duration = Vector3.Distance(characterObj.transform.position, destroyPoint[0]) / 2f;
+                            // 使用 DOTween 让人物移动到销毁点
+                            characterObj.transform.DOMove(destroyPoint[0], duration).SetEase(Ease.Linear).OnComplete(() =>
+                            {
+                                // 销毁人物
+                                Destroy(characterObj);
+                            });
+                        });
+
+                    });
+               // });
+            });
+            yield return new WaitForSeconds(1f);
+        }
+
     }
     public void deletePrefab()
     {
